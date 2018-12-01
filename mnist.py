26482348d6034d66e461b7e112250f958c37f5d9
Steve Payne <github@iamsrp.com>
2018-11-30 22:32:22 -0800

    Use non-deprecated code

--- mnist.py
+++ mnist.py
@@ -18,43 +18,10 @@ import sys
 
 # ----------------------------------------------------------------------
 
-def load_data(mode='train'):
-    """
-    Function to (download and) load the MNIST data
-    :param mode: train or test
-    :return: images and the corresponding labels
-    """
-    from tensorflow.examples.tutorials.mnist import input_data
-    mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
-    if mode == 'train':
-        x_train, y_train, x_valid, y_valid = mnist.train.images, mnist.train.labels, \
-                                             mnist.validation.images, mnist.validation.labels
-        return x_train, y_train, x_valid, y_valid
-    elif mode == 'test':
-        x_test, y_test = mnist.test.images, mnist.test.labels
-    return x_test, y_test
-
-
-def randomize(x, y):
-    """ Randomizes the order of data samples and their corresponding labels"""
-    permutation = np.random.permutation(y.shape[0])
-    shuffled_x = x[permutation, :]
-    shuffled_y = y[permutation]
-    return shuffled_x, shuffled_y
-
-
-def get_next_batch(x, y, start, end):
-    x_batch = x[start:end]
-    y_batch = y[start:end]
-    return x_batch, y_batch
-
-# ----------------------------------------------------------------------
-
 # Hyper-parameters
-epochs = 10             # Total number of training epochs
-batch_size = 100        # Training batch size
-display_freq = 100      # Frequency of displaying the training results
-learning_rate = 0.001   # The optimization initial learning rate
+epochs        =  10     # Total number of training epochs
+batch_size    = 100     # Training batch size
+learning_rate =   0.001 # The optimization initial learning rate
 
 # MNIST config
 img_h = img_w = 28             # MNIST images are 28x28
@@ -62,15 +29,19 @@ img_size_flat = img_h * img_w  # 28x28=784, the total number of pixels
 n_classes = 10                 # Number of classes, one class per digit
 
 # Load MNIST data
-x_train, y_train, x_valid, y_valid = load_data(mode='train')
-print("Size of:")
-print("- Training-set:\t\t{}".format(len(y_train)))
-print("- Validation-set:\t{}".format(len(y_valid)))
+(x_train, y_train),(x_test, y_test) = tf.keras.datasets.mnist.load_data()
+# Convert from 28x28 @ 255 greyscale to 784 @ 1.0
+x_train = x_train.reshape([-1, img_size_flat]) / 255.0
+x_test  = x_test .reshape([-1, img_size_flat]) / 255.0
+# And the labels from indices to one-hot arrays
+y_train = np.eye(n_classes)[y_train]
+y_test  = np.eye(n_classes)[y_test]
 
-print('x_train:\t{}'.format(x_train.shape))
-print('y_train:\t{}'.format(y_train.shape))
-print('x_train:\t{}'.format(x_valid.shape))
-print('y_valid:\t{}'.format(y_valid.shape))
+# What do we have
+print("Size of:")
+print("- Training-set: %s" % str(y_train.shape))
+print('x_train: %s' % str(x_train.shape))
+print('y_train: %s' % str(y_train.shape))
 
 # Create the net graph, first the nodes
 ins  = [Node(node_type=NodeType.IN)  for i in range(img_size_flat)]
@@ -106,7 +77,7 @@ x = in_tensor
 y = tf.placeholder(tf.float32, shape=[None, n_classes], name='Y')
 
 # Define the loss function, optimizer, and accuracy
-loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=y, logits=out_tensor),
+loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits_v2(labels=y, logits=out_tensor),
                       name='loss')
 optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate,
                                    name='Adam-op').minimize(loss)
@@ -133,44 +104,41 @@ sess.run(init)
 # Number of training iterations in each epoch
 num_tr_iter = int(len(y_train) / batch_size)
 for epoch in range(epochs):
-    print('Training epoch: {}'.format(epoch + 1))
-    # Randomly shuffle the training data at the beginning of each epoch 
-    x_train, y_train = randomize(x_train, y_train)
+    print('Training epoch: %d' % (epoch + 1))
+
+    # Randomly shuffle the training data at the beginning of each epoch
+    permutation = np.random.permutation(y_train.shape[0]).astype(np.int32)
+    x_train = x_train[permutation, :]
+    y_train = y_train[permutation]
+
+    pos = 0
     for iteration in range(num_tr_iter):
-        start = iteration * batch_size
-        end = (iteration + 1) * batch_size
-        x_batch, y_batch = get_next_batch(x_train, y_train, start, end)
+        # Slice out this batch
+        x_batch = x_train[pos : pos + batch_size]
+        y_batch = y_train[pos : pos + batch_size]
+        pos    += batch_size
 
         # Run optimization op (backprop)
         feed_dict_batch = {x: x_batch, y: y_batch}
         sess.run(optimizer, feed_dict=feed_dict_batch)
 
-        if iteration % display_freq == 0:
+        if iteration % 100 == 0:
             # Calculate and display the batch loss and accuracy
             loss_batch, acc_batch = sess.run([loss, accuracy],
                                              feed_dict=feed_dict_batch)
 
-            print("iter {0:3d}:\t Loss={1:.2f},\tTraining Accuracy={2:.01%}".
-                  format(iteration, loss_batch, acc_batch))
-
-    # Run validation after every epoch
-    feed_dict_valid = {x: x_valid[:1000], y: y_valid[:1000]}
-    loss_valid, acc_valid = sess.run([loss, accuracy], feed_dict=feed_dict_valid)
-    print('---------------------------------------------------------')
-    print("Epoch: {0}, validation loss: {1:.2f}, validation accuracy: {2:.01%}".
-          format(epoch + 1, loss_valid, acc_valid))
-    print('---------------------------------------------------------')
+            print("iter %03d:\t Loss=%.2f,\tTraining Accuracy=%.1f%%" %
+                  (iteration, loss_batch, acc_batch * 100))
 
 
 # ----------------------------------------------------------------------
 
 # Test the network after training
 # Accuracy
-x_test, y_test = load_data(mode='test')
-feed_dict_test = {x: x_test[:1000], y: y_test[:1000]}
+feed_dict_test = {x: x_test, y: y_test}
 loss_test, acc_test = sess.run([loss, accuracy], feed_dict=feed_dict_test)
 print('---------------------------------------------------------')
-print("Test loss: {0:.2f}, test accuracy: {1:.01%}".format(loss_test, acc_test))
+print("Test loss: %.2f  Test accuracy: %.1f%%" % (loss_test, acc_test * 100))
 print('---------------------------------------------------------')    
 
 # ----------------------------------------------------------------------
